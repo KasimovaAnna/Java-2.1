b23308b1d761306c3c04303aac49576db090c1fa
Anna Kasimova <akasimova-86@yandex.ru>
2021-10-20 22:16:46 +0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int priceTicket = 1000;
         int oneMile = 20;
-        int mileAll = priceTicket/oneMile;
-System.out.println("Ваши мили:" + mileAll);
+        int mileAll = priceTicket / oneMile;
+        System.out.println("Ваши мили:" + mileAll);
 
     }
 }
